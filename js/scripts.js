0dc55a9706475cfbff9d861daaa1c34e08e3ed67
Matt Knutson <mattknutson@Matts-MacBook-Pro.local>
2016-01-22 09:04:47 -0800

    return pong for numbers divisable by 5 and 3 for numbers divisable by 3 in an array.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@
 var count = function(number) {
   var item = [];
   for (var i = 1; i <= number; i++) {
-    if (i % 3 === 0) {
+    if (i % 5 === 0) {
+      item.push('pong');
+    }
+    else if (i % 3 === 0) {
       item.push('ping');
     }
     else {
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,14 @@
 describe('count', function () {
+  it("Will return and array of numbers and specific words (ex. ping, or pong) with the total of the elements in the array equalling the number provided by the user", function() {
+    expect(count(6)).to.eql([1,2,"ping",4,"pong","ping"]);
+  });
+
   it("For any numbers divisable by 3 in the array they will show as 'ping' rather than the number", function() {
     expect(count(3)).to.eql([1,2,"ping"]);
   });
 
-  it("is false for years divisible by 100", function() {
-   expect(leapYear(1900)).to.equal(false);
+  it("For any numbers divisable by 5 in the array they will show as 'pong' rather than the number", function() {
+    expect(count(5)).to.eql([1,2,"ping",4,"pong"]);
   });
 
   it("is true for years divisible by 400", function() {
